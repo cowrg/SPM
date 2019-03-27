ade14d7bf8fb729162f0907bd7d28ec473833294
Brotherhood <alessandro.berti94@hotmail.it>
2019-03-28 00:18:22 +0100

    pthread reduce_OK2

--- Assignment2/main.cpp
+++ Assignment2/main.cpp
@@ -12,15 +12,14 @@ typedef struct{
 } params;
 
 
-int nw = (int) std::thread::hardware_concurrency();
 SafeQueue<int> producerQueue;
 SafeQueue<int> outputQueue;
-std::vector<SafeQueue<params*>> paramsVector(nw);
-std::vector<SafeQueue<int>> partialResult(nw);
+std::vector<SafeQueue<params*>>* paramsVector;
+std::vector<SafeQueue<int>>* partialResult;
 
 void producer(int numTask){
     for(int i=0;i<numTask;i++)
-        producerQueue.safe_push(rand()%10000+8);
+        producerQueue.safe_push(rand()%10000);
     producerQueue.safe_push(EOS);
     return;
 }
@@ -43,23 +42,24 @@ bool isPrime(int x){
 void computePrime(int wid){
     params* p;
     int sum = 0;
-    while( ( p = paramsVector[wid].safe_pop())->start != EOS){
+    while( ( p = (*paramsVector)[wid].safe_pop())->start != EOS){
         sum = 0;
         for(int i=p->start; i<=p->end; i++)
             if(isPrime(i))
                 sum++;
-        partialResult[wid].safe_push(sum);
+        (*partialResult)[wid].safe_push(sum);
     }
     return;
 }
 
-void reducer(){
+void reducer(int nw){
     params* p;
     float binLen;
     int extracted = 0, sum = 0, numBin = 0, start = 0, end = 0;
     std::vector<std::thread> threads(nw);
     for(int wid = 0; wid < threads.size(); wid++)
             threads[wid] = std::thread(computePrime, wid);
+
     while ((extracted = producerQueue.safe_pop()) != EOS){
         sum = 0;
         start = 2;
@@ -69,32 +69,33 @@ void reducer(){
                 end =  extracted;
             else
                 end =  start+(int) binLen-1;
-            *p = params{start, end};
+            p = new params{start, end};
             start = start+binLen;
-            paramsVector[wid].safe_push(p);
+            (*paramsVector)[wid].safe_push(p);
         }
 
         for (int wid = 0; wid < nw; wid++)
-            sum+=partialResult[wid].safe_pop();
+            sum+=(*partialResult)[wid].safe_pop();
         outputQueue.safe_push(sum);
-        //std::cout << sum << std::endl;
     }
-    for (int wid = 0; wid < paramsVector.size(); wid++){
-        *p = params{-1, -1};
-        paramsVector[wid].safe_push(p);
+    for (int wid = 0; wid < paramsVector->size(); wid++){
+        p = new params{-1, -1};
+        (*paramsVector)[wid].safe_push(p);
     }
     for (int wid = 0; wid < threads.size(); wid++){
         threads[wid].join();
     }
-    //delete data structures
     return;
 }
 
 int main(int argc, const char ** argv){
     if(argc > 1){
         int numTask = atoi(argv[1]);
+        int nw = atoi(argv[2]);
+        paramsVector = new std::vector<SafeQueue<params*>>(nw);
+        partialResult = new std::vector<SafeQueue<int>>(nw);
         std::thread producerThread = std::thread(producer, numTask);
-        reducer();
+        reducer(nw);
         producerThread.join();  
     }
     else
