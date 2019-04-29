5ba173a29c40cf831f8b32136ada1399eb1a2e95
Brotherhood <alessandro.berti94@hotmail.it>
2019-04-30 01:13:33 +0200

    E pur si muove

--- Assignment3/gamelife.cpp
+++ Assignment3/gamelife.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 #include <vector>
-#include "/home/alessandro/Desktop/github/SPM/Assignment3/CImg-2.6.0/CImg.h"
+#include "./CImg-2.6.0/CImg.h"
+
+using namespace std;
+using namespace cimg_library;
+
+using board_t = cimg_library::CImg<unsigned char>;
+const unsigned int alive = 000;
+const unsigned int dead  = 255;
+
+int 
+
 
 int main(int argc, char* argv[]){
+		
 	return 0;
 }
