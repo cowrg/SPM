c925f0b188fff6ec71802b3eca4cc24a8f965b5a
Brotherhood <alessandro.berti94@hotmail.it>
2019-05-17 18:49:07 +0200

    pthread

--- Assignment4/google_map_reduce.h
+++ Assignment4/google_map_reduce.h
@@ -1,5 +1,6 @@
+//Authors Alessandro Berti, Eugenio Paluello
+
 #include <iostream>
-#include <string>
 #include <stdlib.h> 
 #include <vector>
 #include <map>
@@ -7,32 +8,36 @@
 #include <math.h>
 #include <chrono>
 #include <functional>
-#include <typeinfo>
 #include <mutex>
 #include <condition_variable>
 
-//Provando a fare tutto senza sincronizzazione, serve un vettore le cui posizioni sono accedute solamente da un unico thread. Ciò significa
-//che avremmo tante posizioni quanti i thread (si pensi a 128 thread) molte delle quali potrebbero essere a 0, occupando posto e appesantendo l'ultima reduce.
-//Nel caso volessimo sincronizzare la struttura, sempre utilizzando una map che permette di prepare il workspace per il thread reduce, dovremmo associare una
-//lock a ciascuna possibile chiave, con lock tutte diverse tra loro (ciascuna per chiave) e utilizzando la funzione push_back di vector.
-//Potremmo mettere i risultati ottenuti dalla local_reduce in un unico vettore (es <p,1> <r,5> <a,4>) ma poi dovremmo (dopo il join dei map_phase thread) fare un sort e dividere il vettore
+using std::chrono::steady_clock;
+
+//Alternative:
+//
+//1) Provando a fare tutto senza sincronizzazione, serve un vettore le cui posizioni sono accedute solamente da un unico thread. Ciò significa
+//che avremmo tante posizioni quanti i thread (si pensi a 128 thread) molte delle quali potrebbero essere a 0, occupando quindi memoria e appesantendo l'ultima reduce.
+//
+//2) Nel caso volessimo sincronizzare la struttura, sempre utilizzando una map che permette di prepare il workspace per il thread reduce, dovremmo associare una
+//lock a ciascuna possibile chiave (->lock tutte diverse tra loro) ed utilizzare la funzione push_back di vector dato che non sappiamo a priori quanto sarà la dimensione di ciascun vector.
+//
+//3) Potremmo inserire i risultati ottenuti dalla local_reduce in un unico vettore (es <p,1> <r,5> <a,4>) ma poi dovremmo (dopo il join dei map_phase thread) fare un sort e dividere il vettore
 //in chunk omogenei (ovvero memorizzare start, end) ma questo implica che dovremmo scorrere l'intero vettore dei risultati e quindi, in realtà, mentre scorriamo con il for avremmo potuto
 //già fare l'ultima reduce (al netto di ottimizzazioni che permetterebbo di individuare sub-array in tempo logaritmico).
-//
+
 template<class T> class GoogleMapReduce{
 	private:
-		long nw, input_size;
+		long nw, input_size, counter_phase;
 		std::vector<T> input;
 		std::vector<std::pair<T,long>> output;
-		std::map<long, std::map<T, std::vector<long>>> dispatcher; //wid -> map of that wid
+		std::map<long, std::map<T, std::vector<long>>> dispatcher; //map an array of homogeneous value (i.e. with the same key) to a logical workspace that will be assigned to the reduce thread denoted by wid_assigned (line 45)
 		std::function<std::pair<T, long> (T)> map_function;
 		std::function<std::pair<T, long> (std::pair<T, long>, std::pair<T, long>)> reduce_function;
 		std::vector<std::thread*> vthreads;
-		std::mutex mutex;
-		std::condition_variable condition;
+		std::mutex mutex, mutex_phase;
+		std::condition_variable counter_phase_cv;
 
 		void map_phase(long start, long end, long actual_wid){
-//			std::cout << "-----------" << actual_wid << " " << start << " " << end << std::endl;
 			long wid_assigned = 0;
 			std::map<T,long> hashmap;
 			std::pair<T,long> pair;
@@ -44,15 +49,9 @@ template<class T> class GoogleMapReduce{
 				std::vector<long> *res_vect = &dispatcher[wid_assigned][t.first];
 				res_vect->resize(this->nw);
 				wid_assigned = select_wid_reduce(t.first);
-				std::cout << " size " << res_vect->size() << std::endl;
 				(*res_vect)[actual_wid] = t.second;
 			}
-			for(auto &it : dispatcher[0]){
-				for(auto &p : it.second){
-					std::cout << "item: " << p << " "; 
-				}
 				
-			}
 			return;
 		}
 
@@ -64,65 +63,60 @@ template<class T> class GoogleMapReduce{
 				sum.second = 0;
 				for(auto i = 0; i < item.second.size(); i++){ //abbiamo taroccato la reduce, sommando sequenzialmente, dato che utilizziamo un singolo thread per eseguire la reduce. Questa scelta è una conseguenza di non aver voluto utilizzare meccanismi di sicronizzazioni. 
 					temp = std::pair(item.first, item.second[i]);
-					std::cout << "bello nvim " << item.second[i] << std::endl;
 					sum = this->reduce_function(sum, temp);
 				}
-				std::unique_lock<std::mutex> lock(mutex);
-				condition.wait(lock, [=]{return true;});
+				std::lock_guard<std::mutex> lock(mutex);
 				output.push_back(sum);
-				condition.notify_one();
 			}
+			return;
+		}
 
+		void thread_code(int start, int end, int actual_wid){
+			auto start_time = std::chrono::high_resolution_clock::now();
+			this->map_phase(start,end,actual_wid);
+			auto elapsed = std::chrono::high_resolution_clock::now() - start_time;
+			std::cout << "Thread: " << actual_wid << " Map Phase: " << std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count() << " usec" << std::endl;
+			std::unique_lock<std::mutex> lck(mutex_phase); //Barrier: wait for each thread have compute the map_phase
+			--counter_phase;
+			counter_phase == 0 ? counter_phase_cv.notify_all() : counter_phase_cv.wait(lck, [=]{return counter_phase == 0;}); 
+			start_time = std::chrono::high_resolution_clock::now();
+			this->reduce_phase(actual_wid);
+			elapsed = std::chrono::high_resolution_clock::now() - start_time;
+			std::cout << "Thread: " << actual_wid << " Reduce Phase: " << std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count() << " usec" << std::endl;
 			return;
 		}
 
 		long select_wid_reduce(T value){
 			long wid_assigned = std::hash<T>()(value)%this->nw;
-			std::cout << "Hash: wid " << wid_assigned << " value " << value << std::endl;
+			//std::cout << "Hash: wid " << wid_assigned << " value " << value << std::endl;
 			return wid_assigned;
 		}
 
 	public:
 		GoogleMapReduce(long nw, 
-				std::vector<T> input, //Should I pass the pointer?
+				std::vector<T> input, 
 				std::function<std::pair<T, long> (T)> map_function,
 				std::function<std::pair<T, long> (std::pair<T, long>, std::pair<T, long>)> reduce_function){
-			this->nw = nw;
+			this->nw = this->counter_phase = nw;
 			this->input = input;
-			this->input_size = input.size();
+			this->input_size = this->input.size();
 			this->map_function = map_function;
 			this->reduce_function = reduce_function;
 			this->vthreads = std::vector<std::thread*>(nw);
 		}
 
 
-		std::vector<std::pair<T,long>> run(){
-			long chunk = ceil(this->input_size/this->nw); //Should be computed on instantiation?
-			std::cout << "Chunk_size: " << chunk << std::endl;
+		std::vector<std::pair<T,long>> run(){ //-----------------------------esco il puntatore?
+			long chunk = ceil(this->input_size/this->nw); 
 			long counter = 0, start = 0, end = 0;
 			while(counter < this->nw){
-				end = (counter == this->nw-1) ? input_size-1 : start+chunk-1;
-//				std::cout << "Start: "<< start << std::endl;
-//				std::cout << "End: " << end << std::endl;
-				vthreads[counter] = new std::thread([=]{map_phase(start, end, counter);});
+				end = (counter == this->nw-1) ? this->input_size-1 : start+chunk-1;
+				vthreads[counter] = new std::thread([=]{thread_code(start, end, counter);});
 				start=end+1;
 				counter++;
 			}
-
 			for(auto i = 0; i < vthreads.size(); i++)
 				vthreads[i]->join();
-
-			std::cout << "------------------------" << std::endl;
-			for(const auto &item : dispatcher)
-				for(const auto &i : item.second)
-					std::cout << i.first << std::endl;
-			//FIX
-			std::vector<std::thread*> rthreads(nw);
-			for(auto i = 0; i < rthreads.size(); i++)
-				rthreads[i] = new std::thread([=]{reduce_phase(i);});
-
-			for(auto i = 0; i < vthreads.size(); i++)
-				rthreads[i]->join();
 
 			return output;
 		}
--- Assignment4/main.cpp
+++ Assignment4/main.cpp
@@ -7,42 +7,76 @@ std::pair<T,long> map(T item){
 
 template<typename T>
 std::pair<T,long> reduce(std::pair<T, long> x, std::pair<T, long> y){
-	if(x.first != y.first){ //to be removed
-		std::cout << "Not equals" << std::endl;
-		exit(EXIT_FAILURE);
-	}
 	return std::make_pair(x.first, x.second+y.second);
 }
 /////////////////////////////////////////////////////////////////////
 
+//
 /////////////////////////////////////////////
-std::vector<int> generator(int size){
+//Random_string() Taken from: https://stackoverflow.com/questions/440133/how-do-i-create-a-random-alpha-numeric-string-in-c
+std::string random_string( size_t length ){
+	auto randchar = []() -> char
+	{
+		const char charset[] =
+			"0123456789"
+			"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+			"abcdefghijklmnopqrstuvwxyz";
+		const size_t max_index = (sizeof(charset) - 1);
+		return charset[ rand() % max_index ];
+	};
+	std::string str(length,0);
+	std::generate_n( str.begin(), length, randchar );
+	return str;
+}
+/////////////////////////////////////////////
+
+
+/////////////////////////////////////////////
+std::vector<int> int_generator(int size){
 	std::vector<int> v(size);
 	for(auto i = 0; i < size; i++)
-		v[i] = i%2;
-	for(auto i : v)
-		std::cout << i << std::endl;
+		v[i] = rand()%23;
+	return v;
+}
+/////////////////////////////////////////////
+
+std::vector<std::string> string_generator(int size){
+	std::vector<std::string> v(size);
+	for(auto i = 0; i < size; i++)
+		v[i] = random_string(5);
 	return v;
 }
 /////////////////////////////////////////////
 
 int main(int argc, char**argv){
-	long nw = 8, map_nw = 4, reduce_nw = 4, size = 16;
+	long nw, size;
+	std::vector<std::pair<std::string, long>> string_out;
+	std::vector<std::pair<int, long>> int_out;
 	if(argc < 2){
 		std::cout << "Usage: " << argv[0] << " nw size" << std::endl;
 		return 1;
 	}
 	nw = atoi(argv[1]);
 	size = atoi(argv[2]);
-//	std::function<std::pair<int, long> (int)> map_function = map<int>;
-//	std::function<std::pair<int, long> (std::pair<int, long>, std::pair<int, long>)> reduce_function = reduce<int>;
-//	GoogleMapReduce<int> gmr(nw, generator(size), map_function, reduce_function); 
-	std::function<std::pair<std::string, long> (std::string)> map_function = map<std::string>;
-	std::function<std::pair<std::string, long> (std::pair<std::string, long>, std::pair<std::string, long>)> reduce_function = reduce<std::string>;
-	std::vector<std::string> vector = {"ciao", "come", "butta", "la", "la", "vita", "ciao", "ciao"};
-	GoogleMapReduce<std::string> gmr(nw, vector, map_function, reduce_function); 
-	std::vector<std::pair<std::string, long>> out = gmr.run();
-	for(auto &item : out)
+	std::function<std::pair<int, long> (int)> map_function = map<int>;
+	std::function<std::pair<int, long> (std::pair<int, long>, std::pair<int, long>)> reduce_function = reduce<int>;
+	std::cout << "------------" << std::endl;
+	GoogleMapReduce<int> gmr(nw, int_generator(size), map_function, reduce_function); 
+	std::cout << "------------" << std::endl;
+	int_out = gmr.run();
+	
+	for(auto &item : int_out)
 		std::cout << "Key: " << item.first << "--- Value: " << item.second << std::endl;
-	return 0;
+	
+	
+//	std::function<std::pair<std::string, long> (std::string)> map_function = map<std::string>;
+//	std::function<std::pair<std::string, long> (std::pair<std::string, long>, std::pair<std::string, long>)> reduce_function = reduce<std::string>;
+	//std::vector<std::string> vector = {"ciao", "come", "butta", "la", "la", "vita", "ciao", "ciao"};
+//	GoogleMapReduce<std::string> gmr(nw, string_generator(size), map_function, reduce_function); 
+//	string_out = gmr.run();
+
+//	
+//	for(auto &item : string_out)
+//		std::cout << "Key: " << item.first << "--- Value: " << item.second << std::endl;
+//	return 0;
 }
